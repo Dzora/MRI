a6046164575539a855b6b57dcf30dae29dbf5527
c14kgn <c14kgn@balin.dklab.cs.umu.se>
2019-04-01 17:35:58 +0200

    Inital work on the dialog and it can read your age

--- Assignment_1/Assignment_1.pml
+++ Assignment_1/Assignment_1.pml
@@ -3,10 +3,17 @@
     <Manifest src="manifest.xml" />
     <BehaviorDescriptions>
         <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="tasks/age" xar="behavior.xar" />
     </BehaviorDescriptions>
-    <Dialogs />
+    <Dialogs>
+        <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
+        <Dialog name="MainDialog" src="MainDialog/MainDialog.dlg" />
+    </Dialogs>
     <Resources />
-    <Topics />
+    <Topics>
+        <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" />
+        <Topic name="MainDialog_enu" src="MainDialog/MainDialog_enu.top" topicName="MainDialog" language="en_US" />
+    </Topics>
     <IgnoredPaths />
     <Translations auto-fill="en_US">
         <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
--- Assignment_1/translations/translation_en_US.ts
+++ Assignment_1/translations/translation_en_US.ts
@@ -1,3 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>tasks/age/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Fuck you</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Fuck you</translation>
+        </message>
+        <message>
+            <source>Noob</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Noob</translation>
+        </message>
+    </context>
+</TS>
